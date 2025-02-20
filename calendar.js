e573c935b62a8bf98ffc1ef070ba505e4542242d
Goldnet <Goldnet.offical@gmail.com>
2025-02-20 14:42:59 +0100

    Create calendar.js

---
+++ calendar.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    if (!localStorage.getItem("loggedIn")) {
+        window.location.href = "index.html";
+    }
+};
+function logout() {
+    localStorage.removeItem("loggedIn");
+    window.location.href = "index.html";
+}
