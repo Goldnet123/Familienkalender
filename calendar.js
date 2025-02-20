860ece792cf50bb745d63207cecf650a2003ca54
Goldnet <Goldnet.offical@gmail.com>
2025-02-20 14:56:29 +0100

    Delete calendar.js

--- calendar.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    if (!localStorage.getItem("loggedIn")) {
-        window.location.assign("kalender.html");
-    }
-};
-function logout() {
-    localStorage.removeItem("loggedIn");
-    window.location.href = "index.html";
-}
